5b3d26f60c3685de9779788278c68d6c246564bf
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2023-02-26 19:13:11 +0400

    Test

--- include/functions/functions.h
+++ include/functions/functions.h
@@ -1,54 +1,60 @@
 #pragma once
+namespace acc {
+	enum AccountType {
+		Payment,
+		Deposit,
+		Credit
+	};
 
-enum AccountType {
-	Payment,
-	Deposit,
-	Credit
-};
+	class Account
+	{
+	private:
+		AccountType type;
+		char FSP[10];
+		float Balance;
+		float Percents;
+		Account(AccountType type, char FSP, float Balance, float Percents);
+	public:
+		void set_percents(AccountType type, int Balance, float Percents) {
+			this->Balance = Balance;
+			if (this->type == Deposit) {
+				Balance += Balance * Percents / 12;
+			};
+			if (this->type == Credit || Balance < 0) {
+				Balance += (-1) * Balance * Percents / 12;
+			};
+		};
+		float get_per() {
+			return Percents;
+		};
+		float get_b() {
+			return Balance;
+		};
+		void set_b(float Balance) {
+			this->Balance = Balance;
+		};
+		Account() {
+			this->type = Payment;
+			this->FSP[10] = 'TDI';
+			this->Balance = 10000;
+			this->Percents = 0;
+		};
+	};
 
-class Account
-{
-private:
-	AccountType type;
-	char FSP;
-	int Balance;
-	float Percents;
-public:
-	void set_percents(int Balance, float Percents) {
-		this->Balance = Balance;
-		if (this->type == Deposit) {
-			Balance +=Balance * Percents / 12;
-		};
-		if (this->type == Credit || Balance < 0) {
-			Balance += (-1) * Balance * Percents / 12;
-		};
+	class AccList {
+	public:
+		static const int CAP = 10;
+	private:
+		Account _Acc[CAP];
+		int _sizes;
+	public:
+		AccList();
+		int size() const;
+		Account operator[](int index) const;
+		void add(Account a);
 	};
-	float get_b() {
-		return Balance;
-	};
-	void set_b(float Balance) {
-		this->Balance = Balance;
-	};
-	Account() {
-		this->type = Payment;
-		this->FSP[10] = 'TDI';
-		this->Balance = 10000;
-		this->Percents = 0;
-	};
-	
-	
-};
-Account MAX_Balance() {
-	Account a[10];
-	int max = 0;
-	int max_i;
-	int i;
-	for (i = 0; i <= 10;i++) {
-		float balance = a[i].get_b();
-		if (balance > max) {
-			max = balance;
-			max_i = i;
-		}
-	}
-	return a[i];
-};
+
+
+
+	int i_max_balance(const AccList& a, const float x);
+}
--- src/functions.cc
+++ src/functions.cc
@@ -1,14 +1,33 @@
 #include <functions/functions.h>
+#include <stdexcept>
+using namespace acc;
+using namespace std;
 
-Account::Account(AccountType type, int Balance) {
-	this->type = type;
-	this->Balance = Balance;
-};
+Account AccList::operator[](int index) const {
+	if (index < 0 || _sizes <= index) {
+		throw out_of_range("[FunctionList::operator[]] Index is out of range");
+	}
 
-int Account{
+}
 
-};
+int i_max_balance(const AccList& a, const float x) {
+	int max_i = -100;
+	float max_balance = 0;
+	const int n = a.size();
+	for (int i = 0; i < n; i++) {
+		const float bal = a[i].get_b();
+		if (max_i == -100 || max_balance < bal) {
+			max_i = i;
+			max_balance = bal;
+		}
+	}
+	return max_i;
+}
 
-int maximum() {
-
+void AccList::add(const Account a) {
+	if (_sizes == CAP) {
+		throw runtime_error("[AccList::add] Capasity is reached.");
+	}
+	_Acc[_sizes] = a;
+	++_sizes;
 }
